49462451665c8ea56475fe8c63ac65122a1c5916
Luke Beckwith <lbeckwit@gmu.edu>
2021-12-08 14:25:04 -0500

    added header to files

--- rtl_src/decomp_map1.v
+++ rtl_src/decomp_map1.v
@@ -1,3 +1,27 @@
+/*
+ * From our research paper "High-Performance Hardware Implementation of CRYSTALS-Dilithium"
+ * by Luke Beckwith, Duc Tri Nguyen, Kris Gaj
+ * at George Mason University, USA
+ * https://eprint.iacr.org/2021/1451.pdf
+ * =============================================================================
+ * Copyright (c) 2021 by Cryptographic Engineering Research Group (CERG)
+ * ECE Department, George Mason University
+ * Fairfax, VA, U.S.A.
+ * Author: Luke Beckwith
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ * @author   Luke Beckwith <lbeckwit@gmu.edu>
+ */
+
+
 `timescale 1ns / 1ps
 
 module decomp_map1(
